01478f1b838b37dd09e0d7ff37850ea712c1030d
Antony Correia <antony.correia@grtgaz.com>
2021-01-20 11:16:25 +0100

    add fxml

---
+++ src/main/java/com/example/FXMLDocumentController.java
@@ -0,0 +1,20 @@
+package com.example ;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+public class FXMLDocumentController {
+
+    @FXML
+    private Label label;
+
+    @FXML
+    private void handleButtonAction(ActionEvent event) {
+        System.out.println("You clicked me!");
+        label.setText("Hello World!");
+    }
+}
