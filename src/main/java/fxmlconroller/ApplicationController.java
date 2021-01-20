ea16eb2787fa46c3ba25471a2ac8fb5e0aa107fe
Antony Correia <antony.correia@grtgaz.com>
2021-01-20 11:34:53 +0100

    test fxml

--- src/main/java/fxmlconroller/ApplicationController.java
+++ src/main/java/fxmlconroller/ApplicationController.java
@@ -1,20 +1,20 @@
-package com.example ;
+package fxmlconroller;
 
-import java.net.URL;
-import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
-public class FXMLDocumentController {
+public class ApplicationController {
+
 
     @FXML
-    private Label label;
+    private Label description;
+    public Button button;
 
     @FXML
     private void handleButtonAction(ActionEvent event) {
         System.out.println("You clicked me!");
-        label.setText("Hello World!");
+        description.setText("Hello World!");
     }
 }
